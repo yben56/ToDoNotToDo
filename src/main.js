ae905a163aecf44282a6bf3e391bb9fb04060406
DESKTOP-U6EJ3LB\benjamin <knockknockinfo@gmail.com>
2023-11-07 01:25:49 +0800

    install bootstrap 5.3.2

--- src/main.js
+++ src/main.js
@@ -1,4 +1,8 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
+//Bootstrap 5.3.2
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/js/bootstrap.js';
+
 createApp(App).mount('#app')
