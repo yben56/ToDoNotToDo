3b414c476cc6437f3bea4d9adcc9056a3cec8a63
DESKTOP-U6EJ3LB\benjamin <knockknockinfo@gmail.com>
2023-11-12 22:57:59 +0800

    add App.scss

--- src/main.js
+++ src/main.js
@@ -1,6 +1,9 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
+//App.scss
+import './assets/styles/App.scss'
+
 //Bootstrap 5.3.2
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap/dist/js/bootstrap.js'
