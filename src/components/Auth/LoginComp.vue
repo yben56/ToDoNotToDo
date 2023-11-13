ab1ce607ed84f63650692908d45cd007d99215fc
DESKTOP-U6EJ3LB\benjamin <knockknockinfo@gmail.com>
2023-11-13 15:30:30 +0800

    update login form with validation

--- src/components/Auth/LoginComp.vue
+++ src/components/Auth/LoginComp.vue
@@ -2,27 +2,16 @@
     <div class="login">   
         <form @submit.prevent="validate">
             <div class="form-group">
-                <label for="exampleFormControlSelect1">Example select</label>
-                <input type="text" class="form-control" v-bind:class="{ 'is-invalid': nameError }" id="name" placeholder="Your name" v-model="name">
-                <div class="invalid-feedback" id="feedback-1" v-if="errors[0]">
-                    {{ errors[0].message }}
-                </div>
+                <label>{{ $t('Email')}}</label>
+                <input type="text" class="form-control" v-bind:class="{ 'is-invalid': emailError }" id="email" v-model="email">
+                <div class="invalid-feedback" id="feedback-1" v-if="errors[0]">{{ errors[0].message }}</div>
             </div>
             <div class="form-group">
-                <label for="exampleFormControlSelect1">Example select</label>
-                <input type="text" class="form-control" v-bind:class="{ 'is-invalid': emailError }" id="email" placeholder="Your email" v-model="email">
-                <div class="invalid-feedback" id="feedback-2" v-if="errors[1]">
-                    {{ errors[1].message }}
-                </div>
+                <label>{{ $t('Password')}}</label>
+                <input type="password" class="form-control" v-bind:class="{ 'is-invalid': passwordError }" id="password" v-model="password">
+                <div class="invalid-feedback" id="feedback-2" v-if="errors[1]">{{ errors[1].message }}</div>
             </div>
-            <div class="form-group">
-                <label for="exampleFormControlSelect1">Example select</label>
-                <textarea class="form-control" v-bind:class="{ 'is-invalid': commentError }" id="comment" placeholder="Your comment" v-model="comment"></textarea>
-                <div class="invalid-feedback" id="feedback-3" v-if="errors[2]">
-                    {{ errors[2].message }}
-                </div>
-            </div>
-            <button class="btn btn-primary" type="submit">Validate</button>
+            <button type="submit" class="btn btn-danger submit">{{ $t('Submit')}}</button>
         </form>
     </div>
 </template>
@@ -31,32 +20,18 @@
 	export default {	
 		data() {
 			return {
-				name: '',
 				email: '',
-				comment: '',
-				nameError: false,
+				password: '',
 				emailError: false,
-				commentError: false,
+				passwordError: false,
 				errors: []
 			}
 		},
 		methods: {
 			validate() {
 				this.errors = [];
-				var len = this.name.length;
-				if (len < 5 || len > 20) {
-					this.nameError = true;
-					this.errors.push({
-						'message': 'Name must be between 5 to 20 characters long.'
-					});
-				} else {
-					document.getElementById('name').className = "form-control is-valid";
-					this.errors.push({
-						'message': 'Validated.'
-					});
-					document.getElementById('feedback-1').className = "valid-feedback";
-				}
-				// email validate
+				
+				//email validate
 				if(this.email.length < 10 || this.email.search('@') == -1) {
 					this.emailError = true;
 					this.errors.push({
@@ -67,22 +42,23 @@
 					this.errors.push({
 						'message': 'Validated.'
 					});
-					document.getElementById('feedback-2').className = "valid-feedback";
+					document.getElementById('feedback-1').className = "valid-feedback";
 				}
-				// comment validate
-				var regex = /^[a-zA-Z0-9 .-]+$/;
-				if(this.comment.length < 20 || this.comment.match(regex) == null) {
-					this.commentError = true;
+				
+				//password validate
+				var regex = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/;
+				if(this.password.length < 8 || this.password.match(regex) == null) {
+					this.passwordError = true;
 					this.errors.push({
-						'field': 'comment',
-						'message': 'Comment must be of 20 characters or more and use of special characters except . and - are restricted'
+						'field': 'password',
+						'message': 'password Minimum eight characters, at least one letter and one number.'
 					});
 				} else {
-					document.getElementById('comment').className = "form-control is-valid";
+					document.getElementById('password').className = "form-control is-valid";
 					this.errors.push({
 						'message': 'Validated.'
 					});
-					document.getElementById('feedback-3').className = "valid-feedback";
+					document.getElementById('feedback-2').className = "valid-feedback";
 				}
 			}
 		}
@@ -91,7 +67,6 @@
 
 <style scoped lang="scss">
 .login {
-    margin-right: 15px;
-    width: 600px;
+	min-width: 300px;
 }
 </style>
