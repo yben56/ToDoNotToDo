2c8ec1617dc9755976a711be815249e515375871
DESKTOP-U6EJ3LB\benjamin <knockknockinfo@gmail.com>
2023-11-20 13:43:42 +0800

    add postman fetch api

--- src/components/Auth/LoginComp.vue
+++ src/components/Auth/LoginComp.vue
@@ -3,13 +3,25 @@
         <form @submit.prevent="validate" class="row g-3">
 			<div class="col-md-12">
 				<label class="form-label">{{ $t('Email') }}</label>
-				<input type="text" class="form-control" v-bind:class="{ 'is-invalid': emailError }" id="email" v-model="email" required>
-				<div class="invalid-feedback" id="feedback-0" v-if="errors[0]">{{ errors[0].message }}</div>
+				<input 
+					type="text"
+					:class="['form-control', {'is-invalid': emailError, 'is-valid': !emailError && submit}]" 
+					ref="email" 
+					v-model="email" 
+					required
+				>
+				<div class="invalid-feedback" v-if="errors[0]">{{ errors[0].message }}</div>
 			</div>
 			<div class="col-md-12">
 				<label class="form-label">{{ $t('Password') }}</label>
-				<input type="password" class="form-control" v-bind:class="{ 'is-invalid': passwordError }" id="password" v-model="password" required>
-				<div class="invalid-feedback" id="feedback-1" v-if="errors[1]">{{ errors[1].message }}</div>
+				<input 
+					type="password" 
+					:class="['form-control', {'is-invalid': passwordError, 'is-valid': !passwordError && submit}]" 
+					ref="password" 
+					v-model="password" 
+					required
+				>
+				<div class="invalid-feedback" v-if="errors[1]">{{ errors[1].message }}</div>
 			</div>
 			<div class="col-md-12">
 				<button type="submit" class="btn btn-danger submit">{{ $t('Submit')}}</button>
@@ -19,56 +31,74 @@
 </template>
 
 <script>
-	export default {	
-		data() {
-			return {
-				email: '',
-				password: '',
-				emailError: false,
-				passwordError: false,
-				errors: []
+import Postman from '/src/js/Postman.js'
+
+export default {	
+	data() {
+		return {
+			email: '',
+			password: '',
+			emailError: false,
+			passwordError: false,
+			errors: [],
+			submit: false,
+		}
+	},
+	methods: {
+		validate() {
+			this.errors = []
+			
+			//email validate
+			if ( this.email.length < 10 || this.email.search('@') == -1 ) {
+				this.emailError = true
+				this.errors.push({
+					'message': this.$t('ErrorInvalidatEmail')
+				})
+			} else {
+				this.$refs.email.className = "form-control is-valid"
+				//this.errors.push({'message': 'Validate'})
+				this.errors.pop()
+			}
+			
+			//password validate
+			var regex = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/
+			if ( this.password.length < 8 || this.password.match(regex) == null ) {
+				this.passwordError = true
+				this.errors.push({
+					'field': 'password',
+					'message': this.$t('ErrorInvalidatPassword')
+				})
+			} else {
+				this.$refs.password.className = "form-control is-valid"
+				//this.errors.push({'message': 'Validate'})
+				this.errors.pop()
+			}
+
+			this.submit = false
+
+			//Only trigger when validation success
+			if ( this.errors.length === 0 ) {
+				this.submitForm()
 			}
 		},
-		methods: {
-			validate() {
-				this.errors = []
-				
-				//email validate
-				if(this.email.length < 10 || this.email.search('@') == -1) {
-					this.emailError = true
-					this.errors.push({
-						'message': this.$t('ErrorInvalidatEmail')
-					});
-				} else {
-					document.getElementById('email').className = "form-control is-valid"
-					this.errors.push({
-						'message': 'Validated.'
-					});
-					document.getElementById('feedback-0').className = "valid-feedback"
-				}
-				
-				//password validate
-				var regex = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/
-				if(this.password.length < 8 || this.password.match(regex) == null) {
-					this.passwordError = true
-					this.errors.push({
-						'field': 'password',
-						'message': this.$t('ErrorInvalidatPassword')
-					});
-				} else {
-					document.getElementById('password').className = "form-control is-valid"
-					this.errors.push({
-						'message': 'Validated.'
-					});
-					document.getElementById('feedback-1').className = "valid-feedback"
-				}
-			}
+		submitForm() {
+			
+			(new Postman().Send(
+				'GET',
+				'/database/List.json',
+				{'a':1}
+			)).then(data => {
+				console.log(data)
+			}).catch(error => {
+				console.error(error);
+			})
 		}
 	}
+}
 </script>
 
 <style scoped lang="scss">
 .login {
-	min-width: 300px;
+	min-width: 300px
 }
 </style>
