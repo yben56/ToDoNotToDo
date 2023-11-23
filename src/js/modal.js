73732338fe5d40c53cb3dd1cb215bd3784cc037a
DESKTOP-U6EJ3LB\benjamin <knockknockinfo@gmail.com>
2023-11-23 10:39:06 +0800

    modal comps to single file and import to main.js

---
+++ src/js/modal.js
@@ -0,0 +1,12 @@
+//Modal
+import ModalComp from '@/components/ModalComp.vue'
+
+//Btn (Modal)
+import AddToDoComp from '@/components/AddToDoComp.vue'
+
+//Auth (Modal)
+import LoginComp from '@/components/Auth/LoginComp.vue'
+import LogoutComp from '@/components/Auth/LogoutComp.vue'
+import SignupComp from '@/components/Auth/SignupComp.vue'
+
+export const comps  = { ModalComp, AddToDoComp, LoginComp, LogoutComp, SignupComp }
--- src/main.js
+++ src/main.js
@@ -18,31 +18,20 @@ app.use(router)
 import { authentication, auth } from '@/js/auth'
 app.use(authentication)
 
-/*Components*/
 //Lang
 app.use(lang)
 app.component('LangComp', LangComp)
 import lang from '@/js/lang.js'
 import LangComp from '@/components/LangComp.vue'
 
-//Axios
+//Axios (ajax)
 import axios from 'axios'
 app.config.globalProperties.$axios = axios
 
-//Modal
-import ModalComp from '@/components/ModalComp.vue'
-app.component('ModalComp', ModalComp)
-
-//Auth (Modal)
-import LoginComp from '@/components/Auth/LoginComp.vue'
-import SignupComp from '@/components/Auth/SignupComp.vue'
-import LogoutComp from '@/components/Auth/LogoutComp.vue'
-app.component('LoginComp', LoginComp)
-app.component('SignupComp', SignupComp)
-app.component('LogoutComp', LogoutComp)
-
-//Btn (Modal)
-import AddToDoComp from '@/components/AddToDoComp.vue'
-app.component('AddToDoComp', AddToDoComp)
+//Modals
+import { comps } from '@/js/modal.js'
+Object.entries(comps).forEach(([name, comp]) => {
+    app.component(name, comp)
+})
 
 app.mount('#app')
